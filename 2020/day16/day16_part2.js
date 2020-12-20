5693924cfe5520da00608fc4d7958fc6e5908642
Alfredo Arranz <alfredo.arranz.alvarado@everis.com>
2020-12-20 13:37:27 +0100

    Corrección dia 16 parte 2

--- 2020/day16/day16_part2.js
+++ 2020/day16/day16_part2.js
@@ -17,13 +17,13 @@ for (let i = 0; i < 20; i++) {
     }
     fields.push(field);
     if (validationRange[0] == null) {
-        validationRange[0] = field.ranges[0]
+        validationRange[0] = [...field.ranges[0]];
     } else {
         validationRange[0][0] > field.ranges[0][0] && (validationRange[0][0] = field.ranges[0][0]);
         validationRange[0][1] < field.ranges[0][1] && (validationRange[0][1] = field.ranges[0][1]);
     }
     if (validationRange[1] == null) {
-        validationRange[1] = field.ranges[1]
+        validationRange[1] = [...field.ranges[1]];
     } else {
         validationRange[1][0] > field.ranges[1][0] && (validationRange[1][0] = field.ranges[1][0]);
         validationRange[1][1] < field.ranges[1][1] && (validationRange[1][1] = field.ranges[1][1]);
@@ -42,19 +42,15 @@ const checkRow = (rowData) => {
     });
     return allValids;
 }
-for (let i = 25; i < input.data.length; i++) {
+for (let i = input.data.length - 1; i >= 25 ; i--) {
     const rowData = input.data[i].split(',').map(aNumberTxt => parseInt(aNumberTxt, 10));
-    console.log('Procesando línea', i);
     if (checkRow(rowData)) {
         rowData.forEach((value, valueIdx) => {
-            // console.log('Procesando posicion', valueIdx);
             fields.forEach(aField => {
                 if (aField.validPos[valueIdx]) {
                     if (value < aField.ranges[0][0] ||
                         (value > aField.ranges[0][1] && value < aField.ranges[1][0]) ||
                         value > aField.ranges[1][1]) {
-                            // console.log('Campo', aField.field, 'no puede estar en posicion', valueIdx);
-                            console.log(aField.field, value, aField.ranges[0][0], aField.ranges[0][1], aField.ranges[1][0], aField.ranges[1][1], valueIdx);
                             delete aField.validPos[valueIdx];
                     }
                 }
@@ -70,12 +66,6 @@ for (let i = 0; i < fields.length - 1; i++) {
         });
     }
 }
-console.log(fields.map(aField => {
-    return {
-        field: aField.field,
-        validPos: Object.keys(aField.validPos).join()
-    }
-}));
 const myTicket = input.data[22].split(',').map(aNumberTxt => parseInt(aNumberTxt));
 let result = 1;
 fields.filter(aField => aField.field.substring(0, 9) === 'departure')
