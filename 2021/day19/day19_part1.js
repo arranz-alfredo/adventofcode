06ff4973e5ec290bb1dc258761b470db78915b20
Alfredo Arranz <alfredo.arranz.alvarado@nttdata.com>
2021-12-21 10:47:34 +0100

    Día 19 - 2021

---
+++ 2021/day19/day19_part1.js
@@ -0,0 +1,82 @@
+const input = require('./input.json');
+const scannerData = [];
+let newScannerData = [];
+input.data.forEach(aRow => {
+    if (aRow === '') {
+        scannerData.push([...newScannerData]);
+        newScannerData = [];
+    } else if (aRow.substring(0, 3) !== '---') {
+        newScannerData.push(JSON.parse(`[${aRow}]`));
+    }
+})
+scannerData.push([...newScannerData]);
+
+const decToRad = (degrees) => degrees * (Math.PI / 180);
+const cos = (degrees) => Math.round(Math.cos(decToRad(degrees)));
+const sin = (degrees) => Math.round(Math.sin(decToRad(degrees)));
+
+function rotate(cube, rx, ry, rz) {
+    var Axx = cos(rz) * cos(ry);
+    var Axy = cos(rz) * sin(ry) * sin(rx) - sin(rz) * cos(rx);
+    var Axz = cos(rz) * sin(ry) * cos(rx) + sin(rz) * sin(rx);
+    var Ayx = sin(rz) * cos(ry);
+    var Ayy = sin(rz) * sin(ry) * sin(rx) + cos(rz) * cos(rx);
+    var Ayz = sin(rz) * sin(ry) * cos(rx) - cos(rz) * sin(rx);
+    var Azx = -sin(ry);
+    var Azy = cos(ry) * sin(rx);
+    var Azz = cos(ry) * cos(rx);
+
+    return cube.map(aPoint => [
+        Axx * aPoint[0] + Axy * aPoint[1] + Axz * aPoint[2],
+        Ayx * aPoint[0] + Ayy * aPoint[1] + Ayz * aPoint[2],
+        Azx * aPoint[0] + Azy * aPoint[1] + Azz * aPoint[2]
+    ]);
+}
+
+function translate(cubeA, cubeB, originA, originB) {
+    const dx = cubeA[originA][0] - cubeB[originB][0];
+    const dy = cubeA[originA][1] - cubeB[originB][1];
+    const dz = cubeA[originA][2] - cubeB[originB][2];
+
+    return cubeB.map(aPoint => [
+        aPoint[0] + dx,
+        aPoint[1] + dy,
+        aPoint[2] + dz
+    ]);
+}
+
+let resultCube = scannerData[0];
+scannerData.splice(0, 1);
+let scannerIdx = 0;
+while (scannerData.length > 0 && scannerIdx < scannerData.length) {
+    let found = false;
+    for (let x = 0; x < 4 && !found; x++) {
+        for (let y = 0; y < 4 && !found; y++) {
+            for (let z = 0; z < 4 && !found; z++) {
+                const rotatedCube = rotate(scannerData[scannerIdx], x * 90, y * 90, z * 90);
+                for (let originAIdx = 0; originAIdx < resultCube.length && !found; originAIdx++) {
+                    for (let originBIdx = 0; originBIdx < rotatedCube.length && !found; originBIdx++) {
+                        const translatedCube = translate(resultCube, rotatedCube, originAIdx, originBIdx);
+                        const cubeCandidate = resultCube.map(aCoord => [aCoord[0], aCoord[1], aCoord[2]]);
+                        translatedCube.forEach (aCoord => {
+                            if (cubeCandidate.findIndex(aCompCoord => aCoord[0] === aCompCoord[0] && aCoord[1] === aCompCoord[1] && aCoord[2] === aCompCoord[2]) === -1) {
+                                cubeCandidate.push([...aCoord]);
+                            }
+                        })
+                        if ((resultCube.length + translatedCube.length  - cubeCandidate.length) >= 12) {
+                            found = true;
+                            resultCube = cubeCandidate;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (found) {
+        scannerData.splice(scannerIdx, 1);
+        scannerIdx = 0;
+    } else {
+        scannerIdx++;
+    }
+}
+console.log(resultCube.length);
