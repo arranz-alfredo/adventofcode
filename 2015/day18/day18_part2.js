ed31d6bb3bfd42e95b2abda1d0ca484f0daa5183
Alfredo Arranz <alfredo.arranz.alvarado@nttdata.com>
2021-12-22 01:09:25 +0100

    Día 18 - 2015

---
+++ 2015/day18/day18_part2.js
@@ -0,0 +1,30 @@
+const input = require('./input.json');
+let grid = input.data.map(aRow => aRow.split('').map(aValue => aValue === '#'));
+
+for (let step = 0; step < 100; step++) {
+    grid = grid.map((aRow, idxY) => aRow.map((isOn, idxX) => {
+        if (
+            (idxY === 0 && idxX === 0) ||
+            (idxY === 0 && idxX === aRow.length - 1) ||
+            (idxY === grid.length - 1 && idxX === 0) ||
+            (idxY === grid.length - 1 && idxX === aRow.length - 1)
+        ) {
+            return true;
+        }
+        let onNeighbours = 0;
+        for (let y = idxY - 1; y <= idxY + 1; y++) {
+            for (let x = idxX - 1; x <= idxX + 1; x++) {
+                if (y >= 0 && y < grid.length && x >= 0 && x < aRow.length && (y !== idxY || x !== idxX) && grid[y][x]) {
+                    onNeighbours += 1
+                }
+            }
+        }
+
+        if (isOn) {
+            return onNeighbours === 2 || onNeighbours === 3;
+        } else {
+            return onNeighbours === 3;
+        }
+    }));
+}
+console.log(grid.reduce((prev, curr) => prev + curr.reduce((prev2, curr2) => prev2 + (curr2 ? 1 : 0), 0), 0));
