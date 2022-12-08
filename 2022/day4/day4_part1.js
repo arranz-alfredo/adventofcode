885fa6042f599f0b3d0d640fbe4cfd6b76d2b05a
Alfredo Arranz <alfredo.arranz.alvarado@nttdata.com>
2022-12-08 01:10:39 +0100

    Día 4 - 2022

---
+++ 2022/day4/day4_part1.js
@@ -0,0 +1,9 @@
+const input = require('./input.json');
+const overlaps = input.data.filter((aPair) => {
+    const elfs = aPair.split(',');
+    const elf1 = elfs[0].split('-').map((aStrNumber) => parseInt(aStrNumber));
+    const elf2 = elfs[1].split('-').map((aStrNumber) => parseInt(aStrNumber));
+    return (elf1[0] >= elf2[0] && elf1[0] <= elf2[1] && elf1[1] >= elf2[0] && elf1[1] <= elf2[1]) ||
+        (elf2[0] >= elf1[0] && elf2[0] <= elf1[1] && elf2[1] >= elf1[0] && elf2[1] <= elf1[1]);
+});
+console.log(overlaps.length);
